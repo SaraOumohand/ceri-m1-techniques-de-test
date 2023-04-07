8b8e50ba2a604c52f8f9d8d416fcf4bdeb3953d3
OUMOHAND Sara <saraoumohand@gmail.com>
2023-04-07 14:15:27 +0200

    partie 01 tests unitaires

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -0,0 +1,14 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IPokemonFactoryTest {
+
+	/*@Test
+	public void test() {
+		fail("Not yet implemented");
+	}*/
+
+}
